6574b632b7c698177a955993876478cd597cc85e
mcgidoug <mcgidoug@gmail.com>
2022-07-28 14:39:29 -0500

    added color randomization functionality

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 // Global selections and variables
+const colorDivs = document.querySelectorAll(".color");
+const generateBtn = document.querySelector(".generate");
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexes = document.querySelectorAll(".color h2");
+let initialColors;
+// FUNCTIONS
 
-// Functions
+// Color Generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    // Add color to background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+  });
+}
+
+randomColors();
