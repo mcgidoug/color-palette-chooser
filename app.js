c3d306121fc708e7c3ec93a071c6406379934fa7
mcgidoug <mcgidoug@gmail.com>
2022-08-09 17:07:07 -0500

    fixed local storage bug

--- app.js
+++ app.js
@@ -262,7 +262,14 @@ function savePalette(e) {
     colors.push(hex.innerText);
   });
   // Generate object
-  let paletteNr = savedPalettes.length;
+  let paletteNr;
+  const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+  if (paletteObjects) {
+    paletteNr = paletteObjects.length;
+  } else {
+    paletteNr = savedPalettes.length;
+  }
+
   const paletteObj = { name, colors, nr: paletteNr };
   savedPalettes.push(paletteObj);
   // Save to local storage
@@ -332,9 +339,10 @@ function closeLibrary() {
 
 function getLocal() {
   if (localStorage.getItem("palettes") === null) {
-    localStorage = [];
+    localPalettes = [];
   } else {
     const paletteObjects = JSON.parse(localStorage.getItem("palettes"));
+    savedPalettes = [...paletteObjects];
     paletteObjects.forEach((paletteObj) => {
       // Generate the palette for the library
       const palette = document.createElement("div");
